d3e4c90dc7810ade63c7b2e82246004ed9ea7645
Sergio Gonzalez <gonzalse@NBSISOPE-ASU01.personal.com.py>
2015-08-05 19:04:13 -0300

    initial

---
+++ testing.py
@@ -0,0 +1,14 @@
+'''
+Created on 3/8/2015
+
+@author: gonzalse
+'''
+
+
+print("hola mundo")
+e=9
+if e != 0 : 
+    print "e es distinto de cero"
+    print "adios"
+elif e == 0 :
+    print "e igual a cero"
