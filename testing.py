d6e6b3449bd23b6d908167f6e7b6146c60c6570f
Sergio Gonzalez <gonzalse@NBSISOPE-ASU01.personal.com.py>
2015-08-06 15:55:10 -0300

    se agrega else igual a cero.

--- testing.py
+++ testing.py
@@ -3,12 +3,20 @@ Created on 3/8/2015
 
 @author: gonzalse
 '''
-
+"""
+test
+"""
 
 print("hola mundo")
-e=9
+"""
+imprime hola mundo en pantalla
+"""
+e=0
 if e != 0 : 
-    print "e es distinto de cero"
+    print "e es distinto de cero (0)"
     print "adios"
 elif e == 0 :
     print "e igual a cero"
+else: print("e es igual a cero")
+
+print("hola test")
